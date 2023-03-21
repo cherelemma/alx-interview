e460149288d994efaad2e01d4ab82dd63dd955c8
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-03-22 02:51:49 +0300

    Update solutions

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,7 +7,7 @@ const filmId = process.argv[2];
 request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
   if (error) return console.log(error);
 
-  const characters = JSON.parse(body).characters;
+  const characters = JSON.parse(body).producer;
 
   for (const character of characters) {
     await new Promise((resolve, reject) => {
